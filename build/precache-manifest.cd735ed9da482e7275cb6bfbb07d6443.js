ab9d583ff240e9cb80a8045a046b7d51257962f5
Saulo Tavares <saulo.11@hotmail.com>
2019-08-02 14:30:45 -0300

    version 1.0.0

---
+++ build/precache-manifest.cd735ed9da482e7275cb6bfbb07d6443.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4d144127c204e944c3e67fc2c4b5b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b827e59c8bcf2590d38",
+    "url": "/static/js/2.165553ab.chunk.js"
+  },
+  {
+    "revision": "7a2ce5ab4b224e5c3e8d",
+    "url": "/static/js/main.1f8d2251.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5f064f55716d7c877ed0752f06cc6c0",
+    "url": "/static/media/facebook.e5f064f5.svg"
+  },
+  {
+    "revision": "16512a6c2a66bcc91af0a20611b693a2",
+    "url": "/static/media/image1.16512a6c.jpg"
+  },
+  {
+    "revision": "a7bd39dab6ad31739271f7731fb6c659",
+    "url": "/static/media/image2.a7bd39da.jpg"
+  },
+  {
+    "revision": "e5a657ffbc3f1a89d3ab8fe1d6ac306f",
+    "url": "/static/media/image3.e5a657ff.jpg"
+  },
+  {
+    "revision": "52373f2ce82e432d4642c912cdb0afb9",
+    "url": "/static/media/image4.52373f2c.jpg"
+  },
+  {
+    "revision": "b4a563e72ffd3b70df26ccf28372221b",
+    "url": "/static/media/instagram.b4a563e7.svg"
+  },
+  {
+    "revision": "afce8e44a407850d8fe4660164eaf762",
+    "url": "/static/media/logoKasivvo.afce8e44.png"
+  },
+  {
+    "revision": "97801018e9e2238aa2db0786f6909e62",
+    "url": "/static/media/mixcloud.97801018.svg"
+  },
+  {
+    "revision": "240d773ed40d22293d3d5900c58ac01a",
+    "url": "/static/media/soundcloud.240d773e.svg"
+  },
+  {
+    "revision": "8cab3679d9f2b00d41134956c82ce52a",
+    "url": "/static/media/youtube.8cab3679.svg"
+  }
+]);
